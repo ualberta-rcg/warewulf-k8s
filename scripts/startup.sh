bcde52ca059727cecb75a7e01b5a1692e94f3572
Karim Ali <kali2@ualberta.ca>
2025-01-26 02:56:51 +0000

    Add namespace and macvlan configs

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-cp -a /build/ww/* /
+if [ -d "/build" ]; then
+
+	cp -a /build/ww/* /
+
+	cp -a /build/overlays/* ${OVERLAYDIR}/
+
+	mkdir /container
+	cp /build/scripts/* /container
+	cp -f /build/scripts/service /usr/sbin/service
+	mkdir -p /usr/share/bash_completion/completions/
+	cp /etc/warewulf/bash_completion.d/* /usr/share/bash_completion/completions/
+	
+	rm -rf /build
+fi
 
 WWCONF=/etc/warewulf/warewulf.conf
 
@@ -11,16 +24,12 @@ yq e '.nfs.enabled = "false"' -i $WWCONF
 
 OVERLAYDIR=`yq '.paths.wwoverlaydir' $WWCONF`
 
-cp -a /build/overlays/* $OVERLAYDIR/
-echo 'munge:x:114:118::/nonexistent:/usr/sbin/nologin' >> /etc/passwd
-chown munge:munge $OVERLAYDIR/slurm/rootfs/etc/munge/munge.key.ww
+useradd -r -d /nonexistent -s /usr/sbin/nologin munge
+useradd -r -d /nonexistent -s /usr/sbin/nologin slurm
 
-mkdir /container
-cp /build/scripts/* /container
-cp -f /build/scripts/service /usr/sbin/service
-mkdir -p /usr/share/bash_completion/completions/
-cp /etc/warewulf/bash_completion.d/* /usr/share/bash_completion/completions/
-
-rm -rf /build
+chown munge:munge ${OVERLAYDIR}/slurm/rootfs/etc/munge/munge.key.ww
+mkdir -p ${OVERLAYDIR}/cvmfs/rootfs/usr/local/bin
+cd ${OVERLAYDIR}/cvmfs/rootfs/usr/local/bin/
+curl -O https://cvmrepo.s3.cern.ch/cvmrepo/apt/cvmfs-release-latest_all.deb
 
 /container/start
